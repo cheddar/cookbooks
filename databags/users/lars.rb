67ea178bbfb693dfbe699477e23c97bbaf649b1a
Benedikt Böhm <bb@xnull.de>
2012-03-25 08:58:54 +0200

    add user lars for chariteam

---
+++ databags/users/lars.rb
@@ -0,0 +1,11 @@
+comment "Lars"
+
+nodes({
+  "test.chariteam.de" => {
+    :tags => %w(hostmaster),
+  },
+})
+
+authorized_keys [
+"ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA8uXWvW577XhRr1gU8XfnSkyoMhkiq2lefYoGjo71Lg6oIb7VvB9RdaDzOBN4rHYJEOcGdSrT9o5W/KF7HGL7FDapgkonvQmD6Ht8eekKe8F2j8PvEqsze+O3JHQU4s1swnrrrdf5fq200fUHjIyUWEsjjNUgdZ5AP8m8gzMSD+/ZRM8y2j9FYexwoRPa/QdCqa/kvKeuuXsvBotA8zq78s4QPG1fEwceJ0J4C1jbT3mIciAdiSe6JmNP5Zx/Hd/gLlmVt65NmzH35eVu7X5fE6qwSuOexsLjih8RGEfnMIEmAaqkVDxW/HHow9w1VugviS9wyKEJ9Tbv9nZUuB65lw== lfk-ssh-for-chariteam",
+]
