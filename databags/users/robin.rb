7be632ce503dfd18281b01e2274de6b6357c91f9
Benedikt Böhm <bb@xnull.de>
2012-05-22 11:41:29 +0200

    add user robin

---
+++ databags/users/robin.rb
@@ -0,0 +1,12 @@
+comment "Robin Spaetling"
+email "robin@chariteam.de"
+
+nodes({
+  "test.chariteam.de" => {
+    :tags => %w(hostmaster),
+  },
+})
+
+authorized_keys [
+"ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAjpvOh73xmBaFD+4YwGHQ9QDY/vfjK6Efkquxy1qJ+VHvJMJN8g/g+u9ReH218f4YxSzHfUwm2hKyOk6UmMKKCHvdCeJiV6FIUrH5RnGqGbxCpVQBvHn5hzD/FcLNRtaMx+5wrpVoK1aAtHE8za0OZX0JmJaHfO7IWHj1JnP9mws="
+]
