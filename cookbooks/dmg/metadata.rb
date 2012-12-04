413533fcc2eab80bb8bfd686aa914392f611215b
Benedikt Böhm <bb@xnull.de>
2012-12-05 00:38:58 +0100

    bump base system cookbooks to v1.0.0

--- cookbooks/dmg/metadata.rb
+++ cookbooks/dmg/metadata.rb
@@ -1,7 +1,11 @@
-maintainer       "Joshua Timberman"
+# encoding: utf-8
+
+description "LWRP to install OS X applications from dmgs"
+
+maintainer "Joshua Timberman"
 maintainer_email "cookbooks@housepub.org"
-license          "Apache 2.0"
-description      "LWRP to install OS X applications from dmgs"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
-supports         "mac_os_x"
+license "Apache 2.0"
+
+version "1.0.0"
+
+supports "mac_os_x"
