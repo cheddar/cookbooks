592065b6e0cbac5cd6add140e7d862cb05fc41ff
Benedikt Böhm <bb@xnull.de>
2013-03-30 13:23:23 +0100

    cleanup shorewall rules

--- cookbooks/shorewall/recipes/rules.rb
+++ cookbooks/shorewall/recipes/rules.rb
@@ -15,19 +15,8 @@ end]
 case node[:fqdn]
 
 when "dev.zentoo.org"
-  # LXC
-  #shorewall_lxc_bridge "lxc" do
-  #  interface "lxc0"
-  #  bridged "eth0"
-  #end
-
   shorewall_interface "nodes" do
     interface "vboxnet0 0.0.0.0 optional"
-  end
-
-  shorewall_rule "chef.zenops.net" do
-    dest "$FW:#{nodes['chef.zenops.net']}"
-    destport "http,https"
   end
 
   shorewall_rule "dev.zentoo.org" do
@@ -38,49 +27,6 @@ when "dev.zentoo.org"
   shorewall6_rule "dev.zentoo.org" do
     dest "$FW:#{nodes6['dev.zentoo.org']}"
     destport "http,https,rsync,3000,8081"
-  end
-
-  shorewall_rule "nagios" do
-    dest "$FW:#{nodes["nagios.zenops.net"]}"
-    destport "http,https,8000"
-  end
-
-  {
-    :subnet => "216.221.226.0/24",
-    :lb1 => "107.21.102.64",
-    :lb2 => "50.17.228.210",
-    :lb3 => "23.23.140.99",
-  }.each do |name, addr|
-    shorewall_rule "no-splunk-out-#{name}" do
-      action "DROP"
-      source "all"
-      dest "net:#{addr}"
-      proto "-"
-    end
-
-    shorewall_rule "no-splunk-in-#{name}" do
-      action "DROP"
-      source "net:#{addr}"
-      dest "all"
-      proto "-"
-    end
-  end
-
-  node.run_state[:nodes].each do |n|
-    shorewall_rule "syslog@#{n[:fqdn]}" do
-      source "$FW:#{n[:primary_ipaddress]}"
-      dest "$FW:#{nodes["nagios.zenops.net"]}"
-      destport "514"
-    end
-  end
-
-  node.run_state[:nodes].each do |n|
-    next unless node[:tags].include?("splunk-forwarder")
-    shorewall_rule "splunk-forwarder@#{n[:fqdn]}" do
-      source "$FW:#{n[:primary_ipaddress]}"
-      dest "$FW:#{nodes["nagios.zenops.net"]}"
-      destport "9997"
-    end
   end
 
 when "zeus.xnull.de"
@@ -140,6 +86,11 @@ when "iberico.kanbanero.net"
   shorewall_rule "staging" do
     dest "$FW:#{nodes['staging.kanbanero.net']}"
     destport "http,https"
+  end
+
+when "chaos.home.xnull.de"
+  shorewall_rule "chaos" do
+    destport "-"
   end
 
 end
