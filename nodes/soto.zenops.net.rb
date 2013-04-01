37b74e72bf818ab2c3ca30b3adb5ff5b54b35157
Benedikt Böhm <bb@xnull.de>
2013-04-01 22:12:04 +0200

    use soto for gitlab testing

--- nodes/soto.zenops.net.rb
+++ nodes/soto.zenops.net.rb
@@ -1,7 +1,10 @@
-chef_environment "production"
+chef_environment "testing"
 
 set[:primary_ipaddress] = "5.9.41.226"
 
+set[:gitlab][:host] = "lab.zenops.de"
+
 run_list(%w(
   role[lxc]
+  recipe[gitlab]
 ))
