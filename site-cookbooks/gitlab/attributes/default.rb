bdd8c2b20d7506229a7f77c0f2106186b4bb2540
Benedikt Böhm <bb@xnull.de>
2013-04-07 15:05:31 +0200

    remove gitlab from soto

--- site-cookbooks/gitlab/attributes/default.rb
+++
@@ -1,7 +0,0 @@
-default[:gitlab][:host] = node[:fqdn]
-default[:gitlab][:email_from] = node[:contacts][:hostmaster]
-default[:gitlab][:support_email] = node[:contacts][:hostmaster]
-
-# unicorn config
-default[:gitlab][:unicorn][:worker_processes] = 4
-default[:gitlab][:unicorn][:timeout] = 30
