e434afb01e62d3c21aacb69453a4b282e088aba3
Benedikt Böhm <bb@xnull.de>
2013-04-20 09:32:56 +0200

    switch boca to row based replication

--- nodes/bocaloves.me.rb
+++ nodes/bocaloves.me.rb
@@ -3,6 +3,7 @@ chef_environment "production"
 set[:primary_ipaddress] = "176.9.124.34"
 
 set[:mysql][:server][:ft_min_word_len] = 3
+set[:mysql][:server][:binlog_format] = 'row'
 
 {
   "htdocs" => "/var/www/localhost/htdocs",
