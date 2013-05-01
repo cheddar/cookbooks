42447db0f5df058b05e83cc233d875eccff3d280
Benedikt Böhm <bb@xnull.de>
2013-05-01 20:00:39 +0200

    update aws credentials

--- nodes/dev.zentoo.org.rb
+++ nodes/dev.zentoo.org.rb
@@ -6,8 +6,8 @@ set[:primary_ip6address] = "2a01:4f8:150:30a3::1"
 set[:primary_interface_bridged] = false
 
 set[:metro][:builds] = ["zentoo", "zentoo-next"]
-set[:metro][:aws_access_key_id] = "AKIAJGUN7XK4VYGLLFPA"
-set[:metro][:aws_secret_access_key] = "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"
+set[:metro][:aws_access_key_id] = "AKIAJ3QVLL4QG5UJFIMQ"
+set[:metro][:aws_secret_access_key] = "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"
 
 run_list(%w(
   role[base]
