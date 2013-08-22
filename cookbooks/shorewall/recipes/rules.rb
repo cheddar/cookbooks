31d8f54144119cf45af9b57662945f81d6f48495
Benedikt Böhm <bb@xnull.de>
2013-08-22 08:27:11 +0200

    cleanup

--- cookbooks/shorewall/recipes/rules.rb
+++ cookbooks/shorewall/recipes/rules.rb
@@ -19,31 +19,4 @@ when "www.zenops.net"
     destport "http,https"
   end
 
-when "zeus.xnull.de"
-  shorewall_rule "helios" do
-    dest "$FW:#{nodes['linux-vserver.org']}"
-    destport "ftp,http,imaps,smtp"
-  end
-
-  shorewall_rule "kali" do
-    dest "$FW:#{nodes['kali.xnull.de']}"
-    destport "ftp,smtp,http,https,pop3,pop3s,imap,imaps"
-  end
-
-  # these nodes are not deployed or managed
-  shorewall_rule "noth" do
-    dest "$FW:188.40.228.146"
-    proto "-"
-  end
-
-  shorewall_rule "nemesis" do
-    dest "$FW:188.40.228.151"
-    proto "-"
-  end
-
-when "chaos.home.xnull.de"
-  shorewall_rule "chaos" do
-    destport "-"
-  end
-
 end
