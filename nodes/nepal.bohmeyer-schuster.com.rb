940e83eb0aecc3c62df83e7c3d4ef6bb1811a3d4
Benedikt Böhm <bb@xnull.de>
2013-09-02 23:13:02 +0200

    update bohmeyer nodes

--- nodes/nepal.bohmeyer-schuster.com.rb
+++ nodes/nepal.bohmeyer-schuster.com.rb
@@ -3,5 +3,7 @@ chef_environment "production"
 set[:primary_ipaddress] = "144.76.99.210"
 
 run_list(%w(
+  role[base]
+  role[bohmeyer-schuster]
   role[nepal]
 ))
--- nodes/web1.bohmeyer-schuster.com.rb
+++ nodes/web1.bohmeyer-schuster.com.rb
@@ -3,6 +3,7 @@ chef_environment "production"
 set[:primary_ipaddress] = "144.76.27.66"
 
 run_list(%w(
+  role[base]
   role[bohmeyer-schuster]
   recipe[mysql::server]
   recipe[mysql::backup]
