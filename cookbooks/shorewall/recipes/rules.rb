951d6c845d602ac1f0af4b6908b3eeb75704abfc
Benedikt Böhm <bb@xnull.de>
2013-09-19 08:32:23 +0200

    add some rules

--- cookbooks/shorewall/recipes/rules.rb
+++ cookbooks/shorewall/recipes/rules.rb
@@ -19,4 +19,30 @@ when "www.zenops.net"
     destport "http,https"
   end
 
+when "chef.zenops.net"
+  shorewall_rule "nginx" do
+    destport "http,https,8443,8000"
+  end
+
+  {
+    :subnet => "216.221.226.0/24",
+    :lb1 => "107.21.102.64",
+    :lb2 => "50.17.228.210",
+    :lb3 => "23.23.140.99",
+  }.each do |name, addr|
+    shorewall_rule "no-splunk-out-#{name}" do
+      action "DROP"
+      source "all"
+      dest "net:#{addr}"
+      proto "-"
+    end
+
+    shorewall_rule "no-splunk-in-#{name}" do
+      action "DROP"
+      source "net:#{addr}"
+      dest "all"
+      proto "-"
+    end
+  end
+
 end
