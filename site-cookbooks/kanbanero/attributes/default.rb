6a4cf1ec2b6e3f82b488598b185d702a4c368672
Benedikt Böhm <bb@xnull.de>
2013-10-03 08:25:24 +0200

    kanbanero: add giacomo to deployers

--- site-cookbooks/kanbanero/attributes/default.rb
+++ site-cookbooks/kanbanero/attributes/default.rb
@@ -2,7 +2,7 @@ default[:kanbanero][:homedir] = "/var/app/kanbanero"
 default[:kanbanero][:port] = "3001"
 default[:kanbanero][:deployers] = [
   :bene,
-  :martin,
+  :giacomo,
   :mike,
   :thomas
 ]
