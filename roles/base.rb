121cc63fbd423359e23283b3b6bf3c00d06af40e
Benedikt Böhm <bb@xnull.de>
2013-10-14 12:08:32 +0200

    remove zentoo-next

--- roles/base.rb
+++ roles/base.rb
@@ -3,9 +3,3 @@ description "base role for all nodes"
 run_list(%w(
   recipe[base]
 ))
-
-override_attributes({
-  :portage => {
-    :SYNC => "rsync://mirror.zenops.net/zentoo-portage-next",
-  },
-})
