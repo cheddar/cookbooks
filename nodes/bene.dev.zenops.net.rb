c9a15f1f3730e673919f3308cc5388b46db06b45
Benedikt Böhm <bb@xnull.de>
2013-10-14 11:55:17 +0200

    update nodes

--- nodes/bene.dev.zenops.net.rb
+++ nodes/bene.dev.zenops.net.rb
@@ -2,15 +2,14 @@ chef_environment "development"
 
 set[:primary_ipaddress] = "144.76.167.231"
 
-set[:metro][:builds] = %w(zentoo zentoo-next)
+set[:metro][:builds] = %w(zentoo)
 set[:mysql][:server][:binlog_format] = 'row'
 
 run_list(%w(
   role[base]
   role[workstation]
+  role[zenops-mirror]
   recipe[mysql::server]
   recipe[mongodb::server]
   recipe[zookeeper]
-  recipe[zenops::ci]
-  recipe[zenops::mirror]
 ))
