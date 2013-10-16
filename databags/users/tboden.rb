14e50d83359164a039132852ae79993d335d2d6a
Benedikt Böhm <bb@xnull.de>
2013-10-16 11:30:59 +0200

    add user tboden

---
+++ databags/users/tboden.rb
@@ -0,0 +1,17 @@
+comment "Tobias Boden"
+email "t.boden@bohmeyer-schuster.com"
+tags %w()
+
+nodes({
+  "web1.bohmeyer-schuster.com" => {
+    :tags => %w(hostmaster),
+  },
+})
+
+# MjcXR2cwZC
+password "$6$ec8113b1$ovjJG54tX4DjYtUwfEY5d/v0nCzzdCYu.nuJGDybuvQZRS/F5LvnfSlXhqM2e4K/nEyfPhlZqLzPqrpabP52n1"
+password1 "$1$8759b3cf$0HoN/EKNk40fQyB7VoAV4/"
+
+authorized_keys [
+"ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEA3MYoEVbiUM0IUmEiL0znfTjE4UlKtk4ONb+V3x49jIE8D+qc+vn0CHdbudcPhZ2qwXnCJbfAaism5ZufhDsVQ4SxcBAAX3LYLnOUiOlo52zD2Gihg0nUMAS8866ZwF7B9SBsFcVhBW7bj0c6PgqoR2yt7XkOtBdRwxxTKqReiwZm5iHgTBKRMb6b8wvcy0NlSMtvjZ+qFC4RRfb9wsz3PmoCP72kekY6yYjb+P1tly8SvV6g/jh0CogW4qGz2bjbkFZa1YzylA+3AhGeBfe8eIhBd1jmhvnJHyXiBgd+PLx8TmijSr4RZDFUc4Df3lmE+SfTE6adwQLgb+pXif7Okw== rsa-key-20131016",
+]
