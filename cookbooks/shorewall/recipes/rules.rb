c6b02c187e79a95c8954d09ecbd505c9ab452f4e
Benedikt Böhm <bb@xnull.de>
2013-11-03 14:14:46 +0100

    update shorewall rules

--- cookbooks/shorewall/recipes/rules.rb
+++ cookbooks/shorewall/recipes/rules.rb
@@ -1,8 +1,29 @@
 # use this recipe to define resources for shorewall rules
 
+{
+  :subnet => "216.221.226.0/24",
+  :lb1 => "107.21.102.64",
+  :lb2 => "50.17.228.210",
+  :lb3 => "23.23.140.99",
+}.each do |name, addr|
+  shorewall_rule "no-splunk-out-#{name}" do
+    action "DROP"
+    source "all"
+    dest "net:#{addr}"
+    proto "-"
+  end
+
+  shorewall_rule "no-splunk-in-#{name}" do
+    action "DROP"
+    source "net:#{addr}"
+    dest "all"
+    proto "-"
+  end
+end
+
 case node[:fqdn]
 
-when "www.zenops.net"
+when "wiki.zenops.net"
   shorewall_rule "default" do
     destport "http,https"
   end
@@ -10,27 +31,6 @@ when "www.zenops.net"
 when "chef.zenops.net"
   shorewall_rule "nginx" do
     destport "http,https,8443,8000"
-  end
-
-  {
-    :subnet => "216.221.226.0/24",
-    :lb1 => "107.21.102.64",
-    :lb2 => "50.17.228.210",
-    :lb3 => "23.23.140.99",
-  }.each do |name, addr|
-    shorewall_rule "no-splunk-out-#{name}" do
-      action "DROP"
-      source "all"
-      dest "net:#{addr}"
-      proto "-"
-    end
-
-    shorewall_rule "no-splunk-in-#{name}" do
-      action "DROP"
-      source "net:#{addr}"
-      dest "all"
-      proto "-"
-    end
   end
 
 end
