fa2a26988a741b4a288b8fb6b7d7fea25e006037
Benedikt Böhm <bb@xnull.de>
2013-11-14 08:40:41 +0100

    update my host

--- nodes/bene.dev.zenops.net.rb
+++ nodes/bene.dev.zenops.net.rb
@@ -3,6 +3,7 @@ chef_environment "development"
 set[:primary_ipaddress] = "144.76.167.231"
 
 set[:metro][:builds] = %w(zentoo)
+set[:mysql][:server][:log_bin] = false
 set[:mysql][:server][:binlog_format] = 'row'
 
 run_list(%w(
