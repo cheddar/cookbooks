f765b8b64ec327cc370b1ca7ea75e1572d01b1d5
Benedikt Boehm <bene@bene.dev.zenops.net>
2013-12-24 22:47:18 +0100

    base: move some cookbooks to role[base]

--- roles/base.rb
+++ roles/base.rb
@@ -2,5 +2,13 @@ description "base role for all nodes"
 
 run_list(%w(
   recipe[base]
+  recipe[bash]
+  recipe[git]
+  recipe[htop]
+  recipe[lftp]
+  recipe[python]
+  recipe[ssh]
+  recipe[tmux]
+  recipe[vim]
   recipe[local::shorewall-rules]
 ))
