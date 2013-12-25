df33d2d73f984949a3411e66bc980f87c91837fc
Benedikt Böhm <bb@xnull.de>
2013-12-25 11:46:02 +0100

    fix tons of tailor warnings

--- cookbooks/vim/attributes/default.rb
+++ cookbooks/vim/attributes/default.rb
@@ -1,27 +1,9 @@
 include_attribute "base"
 
-default[:vim][:rcfile] = if mac_os_x?
-                           "#{node[:homedir]}/.vimrc"
-                         else
-                           root? ? "/etc/vim/vimrc.local" : "#{node[:homedir]}/.vimrc"
-                         end
-
-default[:vim][:rcdir] = if mac_os_x?
-                          "#{node[:homedir]}/.vim"
-                        else
-                          root? ? "/etc/vim" : "#{node[:homedir]}/.vim"
-                        end
-
-default[:vim][:plugins] = {
-  :auto_mkdir => "https://github.com/DataWraith/auto_mkdir",
-  :coffee_script => "https://github.com/kchmck/vim-coffee-script",
-  :endwise => "https://github.com/tpope/vim-endwise",
-  :git => "https://github.com/tpope/vim-git",
-  :haml => "https://github.com/tpope/vim-haml",
-  :javascript => "https://github.com/pangloss/vim-javascript",
-  :markdown => "https://github.com/tpope/vim-markdown",
-  :matchit => "https://github.com/vim-scripts/matchit.zip",
-  :powerline => "https://github.com/skwp/vim-powerline",
-  :ruby => "https://github.com/vim-ruby/vim-ruby",
-  :solarized => "https://github.com/hollow/vim-colors-solarized",
-}
+if root?
+  default[:vim][:rcdir] = "/etc/vim"
+  default[:vim][:rcfile] = "#{node[:vim][:rcdir]}/vimrc.local"
+else
+  default[:vim][:rcdir] = "#{node[:homedir]}/.vim"
+  default[:vim][:rcfile] = "#{node[:homedir]}/.vimrc"
+end
